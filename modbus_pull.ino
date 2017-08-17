41289a139ec9ca1a3d9c91a106a685af9f66dedc
Michael Koppelman <michael@badgerhillbrewing.com>
2017-08-17 14:53:57 -0500

    Got float conversions working
    Got float converstions working and evolved towards something useful.

--- modbus_pull.ino
+++ modbus_pull.ino
@@ -4,23 +4,30 @@
 int             slave_id = 2;
 ModbusMaster    node( slave_id );
 char            msg[512];
-int             delay_time = 28000;
-
-#define MYFLOAT(hi, lo) ((double) ((hi) << 16 | (lo)))
-#define MYINT(hi,lo) ( (uint16_t)((hi << 8 ) + lo ) )
-#define MYDBL(hi,lo) ( (double)((hi << 16 ) + lo ) )
+int             delay_time = 5000;
+int             err_count=0;
 
 void setup() {
-//    Particle.variable("msg", msg );
-
+//  Particle.variable("msg", msg );
 // initialize Modbus communication baud rate
-	node.begin(9600);
-	node.enableTXpin(D7);
+    node.begin(9600);
+    node.enableTXpin(D7);
 //  node.enableDebug();  //Print TX and RX frames out on Serial. Beware, enabling this messes up the timings for RS485 Transactions, causing them to fail.
 	sprintf( msg, "%s", "Starting Modbus Interface" );
 	log_msg( msg );
 }
 
+/* not tested yet */
+void init_modbus() {
+    digitalWrite( D7, LOW );
+    delay( delay_time );
+
+    digitalWrite( D7, HIGH );
+    delay( delay_time );
+    
+    digitalWrite( D7, LOW );
+    delay( delay_time );
+}
 
 void loop() {
 	uint8_t         result;
@@ -28,35 +35,37 @@ void loop() {
 	char            datestring[256];
     char            err[256];
 
-    delay( delay_time );
-    get_gateway_time( datestring );
-    if( strlen(datestring) > 0 )
-        sprintf( msg, "Smart Gateway Time: %s", datestring );
-    else
-        sprintf( msg, "Error talking to slave %i. delay_time = %i", slave_id, delay_time );
-    log_msg( msg );
 
-    get_constant_float();
+    if( err_count > 2 ) {
+        sprintf( msg, "Too many errs (%i), rebooting modbus", err_count );
+	    log_msg( msg );
+        delay( delay_time );
+        init_modbus();
+        delay( delay_time );
+        err_count = 0;
+    }
+    get_gateway_time( datestring );
     delay( delay_time );
-/*    
+
     int qty = 8;
     result = get_modbus_data3( data, 0, qty );
     if ( result == node.ku8MBSuccess ) {
-        for ( int j = 0; j < qty; j++ ) {
-			sprintf( msg, "%X = %X + %X (%i)", data[j], highByte(data[j]), lowByte(data[j]), data[j] );
-			log_msg( msg );
-		}
+        sprintf( msg, "%s, %0.0f, %0.2f, %0.2f, %0.2f",
+            datestring,
+            convert_ints_to_float(&data[0]),
+            convert_ints_to_float(&data[2]),
+            convert_ints_to_float(&data[4]),
+            convert_ints_to_float(&data[6])
+        );
+		log_msg( msg );
     }
     else {
+        err_count++;
         get_modbus_error( result, err );
         sprintf( msg, "Error: %i (%X): %s", result, result, err );
         log_msg( msg );
     }
- */
- 
-//	sprintf( msg, "%s", "Done!" );
-//	log_msg( msg );
-//    delay( delay_time );
+     delay( delay_time );
 }
 
 void get_modbus_error( uint8_t result, char* err ) {
@@ -80,7 +89,9 @@ uint8_t get_modbus_data3( uint16_t data_buffer[], uint16_t maddress, uint16_t mq
 		for ( int j = 0; j < mqty; j++ )
 			data_buffer[j] = node.getResponseBuffer( j );
 	}
-	return( result );
+	node.clearResponseBuffer();
+    node.clearTransmitBuffer();
+    return( result );
 }
 
 uint8_t get_modbus_data4( uint16_t data_buffer[], uint16_t maddress, uint16_t mqty ) {
@@ -91,52 +102,40 @@ uint8_t get_modbus_data4( uint16_t data_buffer[], uint16_t maddress, uint16_t mq
 		for ( int j = 0; j < mqty; j++ )
 			data_buffer[j] = node.getResponseBuffer( j );
 	}
+	node.clearResponseBuffer();
+    node.clearTransmitBuffer();
 	return( result );
 }
 
-/*
-    49001	Current Year		
-	49002	Current Month		
-	49003	Current Day		
-	49004	Current Hour		
-	49005	Current Minute		
-	49006	Current Second		
-	49007	Messages Received		
-	49008	Corrupt Messages Received		
-	49009	Messages Sent with Exception		
-	49010	Messages Sent Count		
-	49011	Valid Messages Ignored		
-	49012	Constant Float 12345.0
- */
- 
 void get_constant_float() {
     uint8_t         result;
-    uint16_t        addr, qty, data[32];
+    uint16_t        addr, qty, data[2];
+    char            err[256];
+
+    addr = 9012;
+    qty = 2;
+    result = get_modbus_data4( data, addr-1, qty );
+    if ( result == node.ku8MBSuccess ) {
+        sprintf( msg, "%x %x %f", data[0], data[1], convert_ints_to_float(&data[0]) );
+        log_msg( msg );
+    }
+    else {
+        err_count++;
+        get_modbus_error( result, err );
+        sprintf( msg, "Error: %i (%X): %s", result, result, err );
+        log_msg( msg );
+    }
+}
+
+void get_msg_stats() {
+    uint8_t         result;
+    uint16_t        addr, qty, data[4];
     char            err[256];
 
     addr = 9007;
-    qty = 8;
+    qty = 4;
     result = get_modbus_data4( data, addr-1, qty );
     if ( result == node.ku8MBSuccess ) {
-/*
-        sprintf( msg, "%i %s %i", 49001, "Current Year",      data[0] );
-     	log_msg( msg );
-
- 	    sprintf( msg, "%i %s %i", 49002, "Current Month",     data[1] );
-     	log_msg( msg );
-
-        sprintf( msg, "%i %s %i", 49003, "Current Day",       data[2] );
-     	log_msg( msg );
-
-        sprintf( msg, "%i %s %i", 49004, "Current Hour",      data[3] );
-     	log_msg( msg );
-
-        sprintf( msg, "%i %s %i", 49005, "Current Minute",    data[4] );
-     	log_msg( msg );
-
-        sprintf( msg, "%i %s %i", 49006, "Current Second",    data[5] );
-     	log_msg( msg );
- */
         sprintf( msg, "\t%i %s %i", 49007, "Messages Received", data[0] );
      	log_msg( msg );
 
@@ -145,15 +144,19 @@ void get_constant_float() {
 
         sprintf( msg, "\t%i %s %i", 49010, "Messages Sent",     data[3] );
      	log_msg( msg );
-
-//  sprintf( msg, "\t%i %s %i", 49012, "Constant Float 12345.0",     data[5] );
-//  log_msg( msg );
     }
     else {
+        err_count++;
         get_modbus_error( result, err );
         sprintf( msg, "Error: %i (%X): %s", result, result, err );
         log_msg( msg );
     }
+}
+
+float convert_ints_to_float( uint16_t* w ) {
+    float new_float;
+    memcpy( &new_float, w, sizeof(float) );
+    return( new_float );
 }
 
 void get_gateway_time( char* date_string ) {
@@ -174,6 +177,7 @@ void get_gateway_time( char* date_string ) {
         );
     }
     else {
+        err_count++;
         sprintf( date_string, "%s", "" );
         get_modbus_error( result, err );
         sprintf( msg, "Error: %i (%X): %s", result, result, err );
@@ -182,7 +186,7 @@ void get_gateway_time( char* date_string ) {
 }
 
 void log_msg( char* msg ) {
-    Serial.println( msg );
+//    Serial.println( msg );
     Particle.publish( "modbus", msg );
-    delay( 1000 );
+    delay( 250 );
 }
